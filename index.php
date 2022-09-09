89ec07e79322464d686a3f28f384f9c652a497e0
Emanuele <103491766+DezVibez@users.noreply.github.com>
2022-09-09 13:59:33 +0200

    render a pagina

--- index.php
+++ index.php
@@ -20,6 +20,8 @@ $cars = new Movie("Cars","John Lasseter", 2006);
 $frozen = new Movie("Frozen","Chris Buck & Jennifer Lee", 2013);
 $tarzan = new Movie("Tarzan","Chris Buck & Kevin Lima", 1999);
 
+$movies = [$cars,$frozen,$tarzan];
+
 
 ?>
 
@@ -30,8 +32,49 @@ $tarzan = new Movie("Tarzan","Chris Buck & Kevin Lima", 1999);
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+
+
 </head>
 <body>
+
+
+<div class="movie-container">
+    
+    <div <?php foreach($movies as $movie)  {   ?> >
+        
+        <ul class="movie">
+            
+            <li>
+                <?php echo $movie->name ; ?>
+            </li>
+                
+            <li>
+                <?php echo $movie->director ; ?>
+            </li>
+                
+            <li>
+                <?php echo $movie->year ; ?>
+            </li>
+                
+        </ul>
+    
+    </div <?php  } ; ?>   >
+
+</div>
+
     
 </body>
 </html>
+
+<style>
+
+
+    .movie{
+        text-align: center;
+        margin: 100px;
+        border: 1px solid black;
+        list-style-type: none;
+        padding: 3rem;
+    }
+
+</style>
